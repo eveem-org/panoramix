93b0ae8fcf2ad875ab3992fc6b3ccfd81107e573
Tomasz Kolinko <kolinko@gmail.com>
2019-10-22 10:40:13 +0200

    add abi parser to supplement

--- utils/supplement.py
+++ utils/supplement.py
@@ -176,33 +176,143 @@ def fetch_db():
     os.remove('tmp.supplement.db.zip')
     print('done.')
 
-def create_db():
-    c = _cursor()
+'''
+    
+    Abi crawler and parser. used to refill supplement.py with new ABI/func definitions.
+    It's used by scripts that are not a part of panoramix repo.
 
-    c.execute('''create TABLE functions
-                 (hash text, name text, folded_name text, params text, cooccurs text)''')
+    The function is here, so people wanting to parse ABIs on their own can use parse_insert_abi
+    implementation as a reference. It handles some unobvious edge-cases, like arrays of tuples.
 
-    c.execute('''create INDEX hash_idx ON functions (hash)''')
+'''
 
-    with open('supplement.json') as f:
-        data = json.loads(f.read())
+def crawl_abis_from_cache():
+    import json
 
-    num = 0
-    for f_hash, val in data.items():
-        for f in val:
-            num += 1
-            print(f'adding {num}...')
-            row = (
-                f_hash,
-                f['name'],
-                f['folded_name'],
-                json.dumps(f['params']),
-                ','.join(f['cooccurs']))
+    import os
+    import urllib
+    import urllib.request
 
-    #       print(row)
+    import time
 
-            c.execute('INSERT INTO functions VALUES (?, ?, ?, ?, ?)', row)
+    import re
 
-    conn.commit()
+    import sqlite3
+
+    import sys
+
+    try:
+        from web3 import Web3
+    except:
+        print("install web3\n\t`pip install web3`") # the only dependency in the project :D
+
+    conn = sqlite3.connect('supplement.db')
+    cursor = conn.cursor()
+
+    conn2 = sqlite3.connect('supp2.db')
+    cursor2 = conn2.cursor()
+
+    def parse_insert_abi(abi):
+
+        def parse_inputs(func_inputs):
+            inputs = []
+            params = []
+            param_counter = 0
+            for r in func_inputs:
+                param_counter += 1
+                type_ = r['type']
+
+                name_ = r['name']
+                if len(name_) == 0:
+                    name_ = 'param'+str(param_counter)
+
+                if name_[0] != '_':
+                    name_ = '_'+name_
+
+                params.append({'type': r['type'], 'name': name_})
+
+                if 'tuple' not in type_:
+                    inputs.append(type_)
+                else:
+                    type_ = f"({parse_inputs(r['components'])[0]})" + type_[5:]
+                    inputs.append(type_)
+
+            return ','.join(inputs), params
+
+        output = {}
+
+        for func in abi:      
+            if func['type'] in ['constructor', 'fallback']:
+                continue
+
+            inputs, params = parse_inputs(func['inputs'])
+
+            fname = f"{func['name']}({inputs})"
+
+            sha3 = Web3.sha3(text=fname).hex()[:10]
+
+            if sha3 in output:
+                print("double declaration for the same hash! {}".format(fname))
+                continue
+
+            output[sha3] = {
+                'name': func['name'],
+                'folded_name': fname,
+                'params': params,
+            }
+
+        for sha3, row in output.items():
+            row['cooccurs'] = list(output.keys())
+            insert_row = (
+                    sha3,
+                    row['name'],
+                    row['folded_name'],
+                    json.dumps(row['params']),
+                    ','.join(row['cooccurs']))
+
+            insert_row2 = (
+                    int(sha3, 16),
+                    row['name'],
+                    row['folded_name'],
+                    json.dumps(row['params']))
+
+            test_hash, test_cooccurs = insert_row[0], insert_row[4]
+
+            cursor.execute('SELECT * from functions where hash=? and cooccurs=?', (test_hash, test_cooccurs))
+            results = cursor.fetchall()
+            if len(results) == 0:
+                print('inserting', sha3, row['folded_name'])
+                cursor.execute('INSERT INTO functions VALUES (?, ?, ?, ?, ?)', insert_row)
+                conn.commit()
+
+            cursor2.execute('SELECT * from functions where hash=?', (insert_row2[0], ))
+            results = cursor2.fetchall()
+            if len(results) == 0:
+                print('inserting2', sha3, row['folded_name'])
+                cursor2.execute('INSERT INTO functions VALUES (?, ?, ?, ?)', insert_row2)
+
+                conn2.commit()
 
 
+    def crawl_cache():
+        idx = 0
+
+        path = './cache_abis/'
+
+        if not os.path.isdir(path):
+            print('dir cache_abis doesn\'t exist. it should be there and it should contain abi files')
+            return
+
+        for fname in os.listdir(path):
+            address = fname[:-4]
+            fname = path + fname
+
+            idx += 1
+            print(idx, address)
+
+            with open(fname) as f:
+                abi = json.loads(f.read())
+                parse_insert_abi(abi)
+
+
+    crawl_cache()
